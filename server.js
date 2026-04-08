e84a55d544e2a622fcba707e936a1d969497e88c
Chetan Dhapkas <chetannnnnd098@gmail.com>
2026-04-08 23:56:38 +0530

    Fixed CORS issue

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://chetandhapkas.github.io",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.options("*", cors());
 app.use(express.json());
 
 // ✅ MongoDB Connection
